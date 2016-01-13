9329124b25936a5c6dccdbcd778487aa612d414d
Luis Guilherme <luisguilherme.ufpi@gmail.com>
2016-01-13 14:09:29 -0300

    Variaveis e comentarios modificados.

--- fcfs.py
+++ fcfs.py
@@ -18,31 +18,35 @@ class FCFS(object):
 			inputs[i].append(int(line.split()[0]))
 			inputs[i].append(int(line.split()[1]))
 
-		# Tempo de retorno
+		# Tempo de retorno medio
 		return_time = 0
-		# Tempo corrente
+
+		# Tempo corrente e usado para calcular o tempo atual
 		current_time = 0
 
+		# Tempo de retorno e usado para calcular o tempo de retorno para cada processo
+		return_time_to_process = 0
+
 		for i,inp in enumerate(inputs):
-			# Primeira entrada nao tem valor anterior
+			# Primeira entrada nao espera nenhum outro processo
 			if i == 0:
-				# Valor anterior vai ser igual a "chegada" + "duracao"
-				current_value = inp[0] + inp[1]
+				# Tempo de retorno do processo e usado para calcular o tempo de retorno de cada processo
+				return_time_to_process = inp[1]
 			else:
 				# Testa se houve tempo ocioso
 				if current_time > inp[0]:
 					# Tempo esperado ate agora + tempo para execucao
-					current_value = current_time - inp[0] + inp[1]
+					return_time_to_process = current_time - inp[0] + inp[1]
 				else:
 					# Tempo corrente atualizado para o tempo de todos processos executados + tempo ocioso
 					current_time = inp[0]
 					# Nao houve espera
-					current_value = 0
+					return_time_to_process = inp[1]
 
 			# Tempo corrente e igual ao somatorio do tempo de execucao de todos os processos ate aqui
 			current_time += inp[1]
 			# Somatorio com o tempo de retorno de cada entrada
-			return_time += current_value
+			return_time += return_time_to_process
 
 
 		# Calculado o tempo de retorno medio
