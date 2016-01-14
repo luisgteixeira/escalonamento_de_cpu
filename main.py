424844e6c5cf10067bdd526228a84ea9bae64638
Luis Guilherme <luisguilherme.ufpi@gmail.com>
2016-01-14 10:02:43 -0300

    Leitura de arquivo modificada.

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys, getopt
+import sys
+from file_manager import *
 from fcfs import *
 # from sjf import *
 # from rr import *
 
-def main(argv):
-    # Recebe o nome do arquivo de entrada
-    inputfile = sys.argv[-1]
+def main():
+    # Carrega o arquivo em uma lista
+    lines = sys.stdin.readlines()
 
-    if(inputfile == sys.argv[0]): # Verifica se algum arquivo foi recebido
-        print('Arquivo nao pode ser lido!!')
-    else:
-        # Abre o arquivo
-        arq = open(inputfile, 'r')
-        # Carrega o arquivo em uma lista
-        lines = arq.readlines()
+    fcfs = FCFS(lines)
+    # sjf = SJF(lines)
+    # rr = RR(lines)
 
-        fcfs = FCFS(lines)
-        # sjf = SJF(lines)
-        # rr = RR(lines)
-
-        print('FCFS', fcfs.output)
-        # print('SJF', sjf.output)
-        # print('RR', rr.output)
-
-        # Fecha o arquivo
-        arq.close()
+    print('FCFS', fcfs.output)
+    # print('SJF', sjf.output)
+    # print('RR', rr.output)
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
